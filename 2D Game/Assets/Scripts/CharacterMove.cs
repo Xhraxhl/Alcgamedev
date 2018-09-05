d7c81eb630e768707acebf99ef53ad7d7cd6739a
Unknown <kralc1975@gmail.com>
2018-09-05 10:28:43 -0600

    Started Project
    Created start of 1st level with a script

---
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	public int moveSpeed = 25;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
