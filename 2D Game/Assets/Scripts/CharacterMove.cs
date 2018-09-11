73de6955b3318c6a9efcdc41840d52a2f0658716
Unknown <kralc1975@gmail.com>
2018-09-11 10:30:59 -0600

    Now can move side to side

--- 2D Game/Assets/Scripts/CharacterMove.cs
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -4,17 +4,60 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
-	public int moveSpeed = 25;
+	// Player movement variables
+	public int MoveSpeed;
+	public float JumpHeight;
+	private bool DoubleJump;
 
+	//Player grounded variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
+	//Non-Stick Player
+	private float MoveVelocity;
+	// Jump function code
+	
 
 	// Use this for initialization
 	void Start () {
-		
+	}
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//jump code
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}	
+
+		//move side to side code
+		//Code makes character move side to side with A and D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(+MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
 		
+
+
+
+		//double jump code
+		if(Grounded)
+			DoubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !DoubleJump && !Grounded){
+			Jump();
+			DoubleJump = true;
+		}
+	}
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
