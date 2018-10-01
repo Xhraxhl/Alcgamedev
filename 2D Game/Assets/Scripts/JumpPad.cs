18bd7fda8e775367ef0030e9296c0b3b00440bc3
Unknown <kralc1975@gmail.com>
2018-10-01 10:09:37 -0600

    UPDATES

---
+++ 2D Game/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour {
+public int JumpForce;
+private void OnTriggerEnter(Collider other)
+	
+	{
+    
+	other.GetComponent<Rigidbody>().AddForce(Vector3.up * JumpForce);
+	
+	}
+
+}
