51fb6a69cb08a737647adea3b85c60cfb413ac45
Unknown <kralc1975@gmail.com>
2018-12-03 10:09:25 -0700

    Level Select
    And redid lvl 1

---
+++ 2D Game/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+	public int Back;
+	public int Level1;
+	public int Level2;
+	public int Level3;
+
+	public void BackButton(){
+		SceneManager.LoadScene(Back);
+	}
+	public void Load1(){
+		SceneManager.LoadScene(Level1);
+	}
+	public void Load2(){
+		SceneManager.LoadScene(Level2);
+	}
+	public void Load3(){
+		SceneManager.LoadScene(Level3);
+	}
+
+
+}
--- 2D Game/Assets/Scripts/MainMenu.cs
+++ 2D Game/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 	public int LevelToLoad;
+	public int SceneSelect;
 	public void LoadLevel(){
 		SceneManager.LoadScene(LevelToLoad);
 	}
 	public void LevelExit(){
 		Application.Quit();
+	}
+	public void SelectLevel(){
+		SceneManager.LoadScene(SceneSelect);
 	}
 }
