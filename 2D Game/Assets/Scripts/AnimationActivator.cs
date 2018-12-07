1df02c7aae433bb71599d7ec21e6d542b549c5d1
Unknown <kralc1975@gmail.com>
2018-12-07 10:29:25 -0700

    IDK
    just dont ask

---
+++ 2D Game/Assets/Scripts/AnimationActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationActivator : MonoBehaviour {
+
+
+    public string AnimationName;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void CallThisFromButton()
+    {
+        anim.Play(AnimationName);
+    }
+}
